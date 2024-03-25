52eb748bf7fbebd1d718d54f4b79d4f2f3ca49f2
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-03-25 21:20:44 +0100

    final commit

---
+++ src/internsData.js
@@ -0,0 +1,69 @@
+const internsData = [
+    {
+      id: 1,
+      name: "Fvour orie",
+      picture: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600",
+      program: "Frontend",
+      attandance: 90,
+      grade: 97,
+    },
+    {
+      id: 2,
+      name: "Stanley Uche",
+      picture: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=600",
+      program: "Frontend",
+      attandance: 71,
+      grade: 93,
+    },
+    {
+      id: 3,
+      name: "Emeka Okafor",
+      picture: "https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=600",
+      program: "Frontend",
+      attandance: 85.8,
+      grade: 92,
+    },
+    {
+      id: 4,
+      name: "Uche Clement",
+      picture: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      program: "Frontend",
+      attandance: 70.8,
+      grade: 87,
+    },
+    {
+      id: 5,
+      name: "Princess Divine",
+      picture: "https://images.pexels.com/photos/3186783/pexels-photo-3186783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      program: "Frontend",
+      attandance: 70.8,
+      grade: 82.7,
+    },
+    {
+      id: 6,
+      name: "Favour Uzoka",
+      picture: "https://images.pexels.com/photos/3613459/pexels-photo-3613459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      program: "Frontend",
+      attandance: 70.8,
+      grade: 60,
+    },
+    {
+      id: 7,
+      name: "Chidi Ezewanma",
+      picture: "https://images.pexels.com/photos/594171/pexels-photo-594171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      program: "Frontend",
+      attandance: 70.8,
+      grade: 59,
+    },
+    {
+      id: 8,
+      name: "Ogbonnaya Joy",
+      picture: "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      program: "Frontend",
+      attandance: 70.8,
+      grade: 55,
+    },
+  ];
+  
+  export default internsData;
+  
